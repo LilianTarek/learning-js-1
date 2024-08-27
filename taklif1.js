d6174cfe05d326a4855116662b375e47d8ba9573
Lilian Tarek <157750503+LilianTarek@users.noreply.github.com>
2024-08-27 19:45:07 +0300

    Add files via upload

---
+++ taklif1.js
@@ -0,0 +1,40 @@
+//start task3
+
+
+console.log("%c ELzero %c Web  %c School","color:red;font-size:40px;",
+    "color:green;font-size:40px;font-weight:bold;",
+    "color:white;background-color:blue;font-size:40px;");
+
+
+// end task3
+// start task4
+console.groupCollapsed("Group 1");
+console.log("Message 1");
+console.log("Message 2");
+console.group("Child Group");
+console.log("Message 1");
+console.log("Message 2");
+console.group("Grand Child Group");
+console.log("Message 1");
+console.log("Message 2");
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.groupCollapsed("Group 2");
+console.log("Message 1");
+console.log("Message 2");
+console.groupEnd();
+// end task4
+// start task5
+console.table(["ELZERO", "Lilian", "Aya","Ahmed","Sameh"]);
+// end task5
+// start task6
+
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+
+/*console.log("Iam In Console");
+document.write("Iam In Page");*/
+
+
+// end task6
